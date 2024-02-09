79d01475854a1e20df669c198fee067959d5db7a
IAM-Rana-Ahmad <r80228927@gmail.com>
2024-02-09 06:04:08 +0500

    little changings

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -157,7 +157,7 @@ const Navbar: React.FC = () => {
                 <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
                     <div className="flex items-center mb-8">
                         <a className="mr-auto text-3xl font-bold leading-none" href="#">
-                            <p className='font-medium text-[1.3vw]'>To Do</p>
+                            <p className='font-medium text-2xl'>To Do</p>
                         </a>
                         <button className="navbar-close">
                             <svg className="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -169,9 +169,11 @@ const Navbar: React.FC = () => {
                     <div className="mt-auto">
                         <div className="pt-6">
                             {
-                                login ? <IoMdPerson size={25} /> : <NavLink to={"/signIn"} className="block px-4 py-3 mb-3  text-xs text-center font-semibold leading-none bg-gray-50 hover:bg-gray-100 rounded-xl">Sign in</NavLink>
+                                login ? <button onClick={handleDeleteUser}  className="block px-4 py-3 mb-3  text-xs text-center font-semibold leading-none bg-gray-400 text-black hover:bg-gray-100 rounded-xl">logOut</button>
+
+                                : <NavLink to={"/signIn"} className="block px-4 py-3 mb-3  text-xs text-center font-semibold leading-none bg-gray-50 hover:bg-gray-100 rounded-xl">Sign in</NavLink>
                             }
-                            <a className="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-blue-600 hover:bg-blue-700  rounded-xl" href="#">Sign Up</a>
+                           
                         </div>
                         <p className="my-4 text-xs text-center text-gray-400">
                             <span>Copyright © 2021</span>
